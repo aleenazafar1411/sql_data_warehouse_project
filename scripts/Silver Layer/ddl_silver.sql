98a926443660aa6cc89f30178150d7c3db711ebf
Aleena Zafar <aleenazafar1411@gmail.com>
2025-11-25 15:55:31 +0500

    Create Silver schema tables with DDL script
    This script creates multiple tables in the 'silver' schema, dropping existing tables if they exist. The tables include customer info, product info, sale details, and ERP-related tables.

--- scripts/Silver Layer/ddl_silver.sql
+++ scripts/Silver Layer/ddl_silver.sql
@@ -1,1 +1,93 @@
+/*
+DDL Script: Create Silver Tables
+Script Purpose:
+This script creates tables in the 'silver' schema, dropping existing tables
+if they already exist.
+Run this script to re-define the DDL structure of 'bronze' Tables
+*/
 
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE silver.crm_cust_info;
+
+go
+CREATE TABLE silver.crm_cust_info(
+    cst_id int,
+    cst_key nvarchar(50),
+    cst_firstname nvarchar(50),
+    cst_lastname nvarchar(50),
+    cst_material_status nvarchar(50),
+    cst_gndr nvarchar(50),
+    cst_create_date date,
+    dwh_create_date datetime2 default getdate()
+);
+go
+  
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+DROP TABLE silver.crm_prd_info;
+
+go
+CREATE TABLE silver.crm_prd_info(
+    prd_id int,
+    prd_key nvarchar(50),
+    prd_nm nvarchar(50),
+    prd_cost int,
+    prd_line nvarchar(50),
+    prd_start_dt datetime,
+    prd_end_dt datetime,
+    dwh_create_date datetime2 default getdate()
+);
+go
+  
+IF OBJECT_ID ('silver.crm_sale_details', 'U') IS NOT NULL
+DROP TABLE silver.crm_sale_details;
+
+go
+CREATE TABLE silver.crm_sale_details(
+    sls_ord_num nvarchar(50),
+    sls_prd_key nvarchar(50),
+    sls_cust_id int,
+    sls_order_dt int,
+    sls_ship_dt int,
+    sls_due_dt int,
+    sls_sales int,
+    sls_quantity int,
+    sls_price int,
+    dwh_create_date datetime2 default getdate()
+);
+go
+  
+IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
+DROP TABLE silver.erp_loc_a101;
+
+go
+CREATE TABLE silver.erp_loc_a101(
+    cid nvarchar(50),
+    cntry nvarchar(50),
+    dwh_create_date datetime2 default getdate()
+);
+go
+  
+IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
+DROP TABLE silver.erp_cust_az12;
+
+go
+CREATE TABLE silver.erp_cust_az12(
+    cid nvarchar(50),
+    bdate date,
+    gen nvarchar(50),
+    dwh_create_date datetime2 default getdate()
+);
+go
+  
+IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+DROP TABLE silver.erp_px_cat_g1v2;
+
+go
+CREATE TABLE silver.erp_px_cat_g1v2(
+    id nvarchar(50),
+    cat nvarchar(50),
+    subcat nvarchar(50),
+    maintenance nvarchar(50),
+    dwh_create_date datetime2 default getdate()
+);
+go
